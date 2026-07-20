c2b18afd16fb24f7b0ce2b49cecfdc9a18eff780
healixhospitalsinfo-web <healixhospitalsinfo@gmail.com>
2026-07-20 16:54:33 +0530

    reviews

--- js/main.js
+++ js/main.js
@@ -92,12 +92,48 @@
 
 
     // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
+    var $testimonialCarousel = $(".testimonial-carousel");
+    $testimonialCarousel.owlCarousel({
         autoplay: true,
         smartSpeed: 1000,
         items: 1,
         dots: true,
         loop: true,
+    });
+
+    $("#testimonialForm").on("submit", function (e) {
+        e.preventDefault();
+
+        var name = $.trim($("#testimonialName").val());
+        var profession = $.trim($("#testimonialProfession").val());
+        var message = $.trim($("#testimonialMessage").val());
+
+        if (!name || !profession || !message) {
+            return;
+        }
+
+        var newSlide = $(
+            '<div class="testimonial-item text-center">' +
+            '  <div class="position-relative mb-5">' +
+            '    <img class="img-fluid rounded-circle mx-auto" src="img/user.jpg" alt="">' +
+            '    <div class="position-absolute top-100 start-50 translate-middle d-flex align-items-center justify-content-center bg-white rounded-circle" style="width: 60px; height: 60px;">' +
+            '      <i class="fa fa-quote-left fa-2x text-primary"></i>' +
+            '    </div>' +
+            '  </div>' +
+            '  <p class="fs-4 fw-normal"></p>' +
+            '  <hr class="w-25 mx-auto">' +
+            '  <h3></h3>' +
+            '  <h6 class="fw-normal text-primary mb-3"></h6>' +
+            '</div>'
+        );
+
+        newSlide.find('p').text(message);
+        newSlide.find('h3').text(name);
+        newSlide.find('h6').text(profession);
+
+        $testimonialCarousel.trigger('add.owl.carousel', [newSlide, 0]).trigger('refresh.owl.carousel');
+        $testimonialCarousel.trigger('to.owl.carousel', [0, 300]);
+        $(this).trigger('reset');
     });
 
     // Gallery carousel
